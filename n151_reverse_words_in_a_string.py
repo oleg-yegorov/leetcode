a10a94e42c4b22d6665335ae463131f6c9c1c044
Oleg Egorov <oleg_egorov@msn.com>
2025-03-06 17:04:11 +0100

    151 Reverse words in a string

---
+++ n151_reverse_words_in_a_string.py
@@ -0,0 +1,47 @@
+import pytest
+
+
+# Given an input string s, reverse the order of the words.
+# A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+# Return a string of the words in reverse order concatenated by a single space.
+# Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should
+# only have a single space separating the words. Do not include any extra spaces.
+class Solution:
+    def reverseWordsFast(self, s: str) -> str:
+        return " ".join(reversed(s.split()))
+
+    def reverseWords(self, s: str) -> str:
+        n = len(s)
+        i = n-1
+        ans = ""
+
+        while i >= 0:
+            while i > -1 and s[i] == ' ':
+                i -= 1
+
+            k = i
+            while i > -1 and s[i] != ' ':
+                i -= 1
+
+            if k < 0:
+                break
+
+            j = i + 1
+
+            while j != k+1:
+                ans += s[j]
+                j += 1
+            ans += " "
+
+        return ans[:-1]
+
+
+
+
+@pytest.mark.parametrize('s, res', [
+    ("a", "a"),
+    ("the sky is blue", "blue is sky the"),
+    ("  hello world  ", "world hello"),
+])
+def test_reverse_words(s: str, res: str):
+    assert Solution().reverseWordsFast(s) == res
