94935bd2efb764074ec453681cfeae5fc9ccf04d
Oleg Egorov <oleg_egorov@msn.com>
2025-05-07 19:01:11 +0200

    242 Valid Anagram

---
+++ n242_valid_anagram.py
@@ -0,0 +1,15 @@
+import pytest
+from collections import Counter
+
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
+
+@pytest.mark.parametrize('s, t, res', [
+    ('anagram', 'nagaram', True),
+    ('rat', 'car', False),
+])
+def test_is_anagram(s, t, res):
+    assert Solution().isAnagram(s, t) == res
