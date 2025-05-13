9ac1c55b2e8396656edf96888a7fc30455e71661
Oleg Egorov <oleg_egorov@msn.com>
2025-05-13 19:15:59 +0200

    92 Reverse linked list II

---
+++ n92_reverse_linked_list_ii.py
@@ -0,0 +1,84 @@
+from typing import Optional
+
+
+# Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes
+# of the list from position left to position right, and return the reversed list.
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        # in-place
+        dummy = ListNode(0)
+        dummy.next = head
+
+        # get to the element before left
+        prev = dummy
+        for _ in range(left - 1):
+            prev = prev.next
+        last = prev.next
+
+        cur = last.next
+        for _ in range(right - left):
+            last.next = cur.next
+            cur.next = prev.next
+            prev.next = cur
+            cur = last.next
+
+        return dummy.next
+
+    def reverseBetween2(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        cur_new = dummy
+
+        # add nodes before interval and the first element of the interval, cur_new points to the last-1 element
+        cur = head
+        for _ in range(left - 1):
+            cur_new.next = ListNode(cur.val)
+            cur_new = cur_new.next
+            cur = cur.next
+        cur_new.next = ListNode(cur.val)
+        cur = cur.next
+
+        # insert all elements in interval after left-1 element (= in reverse order)
+        last = cur_new.next
+        for _ in range(right - left):
+            new_node = ListNode(cur.val)
+            new_node.next = cur_new.next
+            cur_new.next = new_node
+            cur = cur.next
+
+        # insert other elements
+        while cur:
+            last.next = ListNode(cur.val)
+            last = last.next
+            cur = cur.next
+
+        return dummy.next
+
+
+def test_reverse_between():
+    l = ListNode(1)
+    l.next = ListNode(2)
+    l.next.next = ListNode(3)
+    l.next.next.next = ListNode(4)
+    l.next.next.next.next = ListNode(5)
+
+    r = Solution().reverseBetween(l, 2, 4)
+
+    assert r.val == 1
+    assert r.next.val == 4
+    assert r.next.next.val == 3
+    assert r.next.next.next.val == 2
+    assert r.next.next.next.next.val == 5
+
+    l = ListNode(1)
+    l.next = ListNode(2)
+    l = Solution().reverseBetween(l, 1, 2)
+    assert l.val == 2
+    assert l.next.val == 1
