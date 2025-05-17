7feacf2212cb214dda3f885bb76fc12a9876f4a2
Oleg Egorov <oleg_egorov@msn.com>
2025-05-17 16:42:25 +0200

    100 Same tree

---
+++ n100_same_tree.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        elif p and q and p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right):
+            return True
+        else:
+            return False
+
+
+def test_same_tree():
+    assert Solution().isSameTree(TreeNode(1, TreeNode(2), TreeNode(3)), TreeNode(1, TreeNode(2), TreeNode(3)))
+    assert not Solution().isSameTree(TreeNode(1, TreeNode(2), None), TreeNode(1, None, TreeNode(2)))
+    assert not Solution().isSameTree(TreeNode(1, TreeNode(2), TreeNode(1)), TreeNode(1, TreeNode(1), TreeNode(2)))
