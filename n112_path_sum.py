627a2138f39c5765df28374824b32871f1693e28
Oleg Egorov <oleg_egorov@msn.com>
2025-05-20 15:29:52 +0200

    112 Path sum

---
+++ n112_path_sum.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+
+        targetSum -= root.val
+        if targetSum == 0 and not root.left and not root.right:
+            return True
+
+        left = self.hasPathSum(root.left, targetSum) if root.left else False
+        if left:
+            return True
+
+        right = self.hasPathSum(root.right, targetSum) if root.right else False
+        return True if right else False
+
+
+def test_has_path_sum():
+    root = TreeNode(5)
+    root.left = TreeNode(4)
+    root.right = TreeNode(8)
+    root.left.left = TreeNode(11)
+    root.left.left.left = TreeNode(7)
+    root.left.left.right = TreeNode(2)
+    root.right.left = TreeNode(13)
+    root.right.right = TreeNode(4)
+    root.right.right.right = TreeNode(1)
+
+    assert Solution().hasPathSum(root, 22)
+
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+
+    assert not Solution().hasPathSum(root, 1)
