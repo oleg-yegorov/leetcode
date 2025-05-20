83b228fe05866efca44e6fd0cb3b049c1d6b19bc
Oleg Egorov <oleg_egorov@msn.com>
2025-05-20 15:13:22 +0200

    114 Flattern binary tree to linked list

---
+++ n114_flattern_binary_tree_to_linked_list.py
@@ -0,0 +1,60 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        def _flattern(node: TreeNode) -> TreeNode:
+            if node.right and node.left:
+                tmp = node.left
+                node.left = node.right
+                node.right = tmp
+
+                last_node = _flattern(node.right)
+                last_node.right = node.left
+                last_node = _flattern(node.left)
+                node.left = None
+                return last_node
+            elif node.left:
+                node.right = node.left
+                node.left = None
+                return _flattern(node.right)
+            elif node.right:
+                return _flattern(node.right)
+            else:
+                return node
+
+        if root:
+            _flattern(root)
+
+
+def test_flattern_tree():
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.left.left = TreeNode(3)
+    root.left.right = TreeNode(4)
+    root.right = TreeNode(5)
+    root.right.right = TreeNode(6)
+
+    Solution().flatten(root)
+    assert root.val == 1
+    assert root.right.val == 2
+    assert root.right.right.val == 3
+    assert root.right.right.right.val == 4
+    assert root.right.right.right.right.val == 5
+    assert root.right.right.right.right.right.val == 6
+
+    root = None
+    Solution().flatten(root)
+    assert root is None
+
+    root = TreeNode(0)
+    Solution().flatten(root)
+    assert root.val == 0
