4431b4a6587dcba0acb948f49f4f3c4feb986e54
Oleg Egorov <oleg_egorov@msn.com>
2025-05-20 17:42:43 +0200

    129 Sum root to leaf numbers

---
+++ n129_sum_root_to_leat_numbers.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def sum_numbers(root: TreeNode, val) -> int:
+            if not root.left and not root.right:
+                return val + root.val
+
+            left_subtree = sum_numbers(root.left, (val + root.val) * 10) if root.left else 0
+            right_subtree = sum_numbers(root.right, (val + root.val) * 10) if root.right else 0
+
+            return left_subtree + right_subtree
+
+        if not root:
+            return 0
+        else:
+            return sum_numbers(root, 0)
+
+
+def test_sum_numbers():
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+
+    assert Solution().sumNumbers(root) == 25
