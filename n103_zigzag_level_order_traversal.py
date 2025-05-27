ef2d04f858a46917155d2b0566021595ddd3825f
Oleg Egorov <oleg_egorov@msn.com>
2025-05-27 15:43:14 +0200

    103 Zigzag level order traversal

---
+++ n103_zigzag_level_order_traversal.py
@@ -0,0 +1,42 @@
+from typing import Optional, List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        level_nodes = [[root]]
+        level_val = [[root.val]]
+        reverse = True
+        while level_nodes[-1]:
+            new_level_val = []
+            new_level_nodes = []
+            for node in level_nodes[-1]:
+                if node.left:
+                    new_level_val.append(node.left.val)
+                    new_level_nodes.append(node.left)
+                if node.right:
+                    new_level_val.append(node.right.val)
+                    new_level_nodes.append(node.right)
+            level_val.append(list(reversed(new_level_val)) if reverse else new_level_val)
+            reverse = not reverse
+            level_nodes.append(new_level_nodes)
+        return level_val[:-1]
+
+
+def test_level_order():
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    assert Solution().levelOrder(root) == [[3], [20,9], [15, 7]]
