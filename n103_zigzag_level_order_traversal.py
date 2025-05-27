0cfda5b70257d25a1b160c7984ef694eb2e066b9
Oleg Egorov <oleg_egorov@msn.com>
2025-05-27 22:12:54 +0200

    103 Zigzag level order traversal

--- n103_zigzag_level_order_traversal.py
+++ n103_zigzag_level_order_traversal.py
@@ -11,27 +11,7 @@ class TreeNode:
 
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
-
-        level_nodes = [[root]]
-        level_val = [[root.val]]
-        reverse = True
-        while level_nodes[-1]:
-            new_level_val = []
-            new_level_nodes = []
-            for node in level_nodes[-1]:
-                if node.left:
-                    new_level_val.append(node.left.val)
-                    new_level_nodes.append(node.left)
-                if node.right:
-                    new_level_val.append(node.right.val)
-                    new_level_nodes.append(node.right)
-            level_val.append(list(reversed(new_level_val)) if reverse else new_level_val)
-            reverse = not reverse
-            level_nodes.append(new_level_nodes)
-        return level_val[:-1]
-
+_
 
 def test_level_order():
     root = TreeNode(3)
